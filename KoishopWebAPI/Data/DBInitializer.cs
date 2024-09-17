f849036ec3e71f3bc4fd842dca061f1d1a4e9cab
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-09-17 20:52:06 +0700

    update

---
+++ KoishopWebAPI/Data/DBInitializer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KoishopBusinessObjects;
+using KoishopRepositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace KoishopWebAPI.Data
+{
+  public class DBInitializer
+  {
+    public static async Task Initialize(KoishopDBContext context, UserManager<User> userManager)
+    {
+      if (!userManager.Users.Any())
+      {
+        var user = new User
+        {
+          UserName = "bob",
+          Email = "bobtest@gmail.com"
+        };
+
+        await userManager.CreateAsync(user, "Pa$$w0rd");
+        await userManager.AddToRoleAsync(user, "Customer");
+        var admin = new User
+        {
+          UserName = "admin",
+          Email = "admin@gmail.com"
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, new[] { "Customer", "Admin" });
+      }
+
+    }
+  }
+}
--- KoishopWebAPI/Program.cs
+++ KoishopWebAPI/Program.cs
@@ -1,17 +1,74 @@
+using System.Text;
+using KoishopBusinessObjects;
+using KoishopRepositories;
+using KoishopWebAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowLocalhost3000",
+      builder => builder
+          .AllowAnyHeader()
+          .AllowAnyMethod()
+          .AllowCredentials()
+          .WithExposedHeaders("Pagination")
+          .WithOrigins("http://localhost:3000"));
+});
 
+builder.Services.AddControllers();
+builder.Services.AddDbContext<KoishopDBContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString"), b => b.MigrationsAssembly("KoishopWebAPI")));
+
+builder.Services.AddIdentity<User, Role>().AddEntityFrameworkStores<KoishopDBContext>();
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+  .AddJwtBearer(options =>
+  {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidateIssuer = false,
+      ValidateAudience = false,
+      ValidateLifetime = true,
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+    };
+  });
+
+builder.Services.AddAuthorization(options =>
+{
+  options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+  options.AddPolicy("RequireStaffRole", policy => policy.RequireRole("Staff"));
+  options.AddPolicy("RequireCustomerRole", policy => policy.RequireRole("Customer"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<KoishopDBContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+try
+{
+  await DBInitializer.Initialize(context, userManager);
+}
+catch (Exception ex)
+{
+  logger.LogError(ex, "A problem occurred during migration");
+}
 
 app.Run();
