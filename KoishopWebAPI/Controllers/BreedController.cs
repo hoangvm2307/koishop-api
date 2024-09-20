e35356971b1bf197ac8bdcfb1a6c9dc47b3f7cb9
MSI\huyli <huylinhit@gmail.com>
2024-09-20 08:29:05 +0700

    Update Controller

---
+++ KoishopWebAPI/Controllers/BreedController.cs
@@ -0,0 +1,58 @@
+﻿using DTOs.Breed;
+using KoishopServices.Interfaces;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoishopWebAPI.Controllers;
+
+public class BreedController : BaseApiController
+{
+    private readonly IBreedService _breedService;
+
+    public BreedController(IBreedService breedService)
+    {
+        _breedService = breedService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<BreedDto>>> GetBreeds()
+    {
+        var breeds = await _breedService.GetListBreed();
+        return Ok(breeds);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateBreed(BreedCreationDto breedCreationDto)
+    {
+        await _breedService.AddBreed(breedCreationDto);
+        return CreatedAtAction(nameof(GetBreeds), breedCreationDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BreedDto>> GetBreedById(int id)
+    {
+        var breed = await _breedService.GetBreedById(id);
+        if (breed == null)
+            return NotFound();
+        return Ok(breed);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateBreed(int id, BreedUpdateDto breedUpdateDto)
+    {
+        var isUpdated = await _breedService.UpdateBreed(id, breedUpdateDto);
+        if (!isUpdated)
+            return NotFound();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteBreed(int id)
+    {
+        var isDeleted = await _breedService.RemoveBreed(id);
+        if (!isDeleted)
+            return NotFound();
+        return NoContent();
+    }
+}
---
+++ KoishopWebAPI/Controllers/ConsignmentController.cs
@@ -0,0 +1,57 @@
+﻿using DTOs.Consignment;
+using KoishopServices.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoishopWebAPI.Controllers;
+
+public class ConsignmentController : BaseApiController
+{
+    private readonly IConsignmentService _consignmentService;
+
+    public ConsignmentController(IConsignmentService consignmentService)
+    {
+        _consignmentService = consignmentService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ConsignmentDto>>> GetConsignments()
+    {
+        var consignments = await _consignmentService.GetListConsignment();
+        return Ok(consignments);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateConsignment(ConsignmentCreationDto consignmentCreationDto)
+    {
+        await _consignmentService.AddConsignment(consignmentCreationDto);
+        return CreatedAtAction(nameof(GetConsignments), consignmentCreationDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ConsignmentDto>> GetConsignmentById(int id)
+    {
+        var consignment = await _consignmentService.GetConsignmentById(id);
+        if (consignment == null)
+            return NotFound();
+        return Ok(consignment);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateConsignment(int id, ConsignmentUpdateDto consignmentUpdateDto)
+    {
+        var isUpdated = await _consignmentService.UpdateConsignment(id, consignmentUpdateDto);
+        if (!isUpdated)
+            return NotFound();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteConsignment(int id)
+    {
+        var isDeleted = await _consignmentService.RemoveConsignment(id);
+        if (!isDeleted)
+            return NotFound();
+        return NoContent();
+    }
+}
