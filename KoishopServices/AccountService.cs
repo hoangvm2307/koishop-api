21d102425cd13ca1aa885b316d8b21526cc88ee4
MSI\huyli <huylinhit@gmail.com>
2024-09-20 08:28:14 +0700

    Fix Bug Login TODO

--- KoishopServices/AccountService.cs
+++ KoishopServices/AccountService.cs
@@ -4,32 +4,41 @@ using Microsoft.AspNetCore.Identity;
 
 namespace KoishopServices
 {
-  public class AccountService 
-  {
-    // public AccountService(UserManager<User> userManager, TokenService tokenService)
-    // public Task<UserDto> Login(string username, string password)
-    // {
-    //     var user = await _userManager.FindByNameAsync(loginDto.Username);
-    //     if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
-    //         return Unauthorized();
+    public class AccountService : IAccountService
+    {
+        // public AccountService(UserManager<User> userManager, TokenService tokenService)
+        // public Task<UserDto> Login(string username, string password)
+        // {
+        //     var user = await _userManager.FindByNameAsync(loginDto.Username);
+        //     if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+        //         return Unauthorized();
 
-    //     var userBasket = await RetrieveBasket(loginDto.Username);
-    //     var anonBasket = await RetrieveBasket(Request.Cookies["buyerId"]);
+        //     var userBasket = await RetrieveBasket(loginDto.Username);
+        //     var anonBasket = await RetrieveBasket(Request.Cookies["buyerId"]);
 
-    //     if (anonBasket != null)
-    //     {
-    //         if (userBasket != null) _context.Baskets.Remove(userBasket);
-    //         anonBasket.BuyerId = user.UserName;
-    //         Response.Cookies.Delete("buyerId");
-    //         await _context.SaveChangesAsync();
-    //     }
+        //     if (anonBasket != null)
+        //     {
+        //         if (userBasket != null) _context.Baskets.Remove(userBasket);
+        //         anonBasket.BuyerId = user.UserName;
+        //         Response.Cookies.Delete("buyerId");
+        //         await _context.SaveChangesAsync();
+        //     }
 
-    //     return new UserDto
-    //     {
-    //         Email = user.Email,
-    //         Token = await _tokenService.GenerateToken(user),
-    //         Basket = anonBasket != null ? anonBasket.MapBasketToDto() : userBasket?.MapBasketToDto()
-    //     };
-    // }
-  }
+        //     return new UserDto
+        //     {
+        //         Email = user.Email,
+        //         Token = await _tokenService.GenerateToken(user),
+        //         Basket = anonBasket != null ? anonBasket.MapBasketToDto() : userBasket?.MapBasketToDto()
+        //     };
+        // }
+        public Task<UserDto> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Login(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
