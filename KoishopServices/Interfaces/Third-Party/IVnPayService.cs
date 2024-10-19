accf0b050e4d16dfbf8c58dcbc83c4a36904dc5b
NemissU <namphse171651@fpt.edu.vn>
2024-10-19 20:46:36 +0700

    Nam: Demo POST Order, integrate VnPay

---
+++ KoishopServices/Interfaces/Third-Party/IVnPayService.cs
@@ -0,0 +1,16 @@
+﻿using KoishopBusinessObjects.VnPayModel;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoishopServices.Interfaces.Third_Party
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
--- KoishopServices/Services/OrderService.cs
+++ KoishopServices/Services/OrderService.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
 using DTOs.Order;
 using KoishopBusinessObjects;
+using KoishopBusinessObjects.Constants;
+using KoishopBusinessObjects.VnPayModel;
 using KoishopRepositories.Interfaces;
+using KoishopServices.Common.Exceptions;
 using KoishopServices.Interfaces;
+using KoishopServices.Interfaces.Third_Party;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,24 +20,74 @@ namespace KoishopServices.Services;
 public class OrderService : IOrderService
 {
     private readonly IMapper _mapper;
+    private readonly UserManager<User> _userManager;
+    private readonly IKoiFishRepository _koiFishRepository;
+    private readonly IOrderItemRepository _orderItemRepository;
     private readonly IOrderRepository _orderRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IVnPayService _vnPayService;
 
-    public OrderService(IMapper mapper, IOrderRepository orderRepository)
+
+    public OrderService(IMapper mapper
+        , IOrderRepository orderRepository
+        , UserManager<User> userManager
+        , IOrderItemRepository orderItemRepository
+        , IKoiFishRepository koiFishRepository
+        , IVnPayService vnPayService
+        , IHttpContextAccessor httpContextAccessor)
     {
         this._mapper = mapper;
+        this._userManager = userManager;
         this._orderRepository = orderRepository;
+        this._orderItemRepository = orderItemRepository;
+        this._koiFishRepository = koiFishRepository;
+        this._vnPayService = vnPayService;
+        this._httpContextAccessor = httpContextAccessor;
     }
-    public async Task AddOrder(OrderCreationDto orderCreationDto)
+    public async Task<string> AddOrder(OrderCreationDto orderCreationDto)
     {
         //TODO: Add validation before create and mapping
-        if ( string.IsNullOrEmpty(orderCreationDto.Status))
+        var customer = await _userManager.FindByIdAsync(orderCreationDto.UserId.ToString());
+        if (customer == null)
         {
-            throw new ArgumentException("Status is required!");
+            throw new NotFoundException(ExceptionConstants.USER_NOT_EXIST);
+        }
+        var order = _mapper.Map<Order>(orderCreationDto);
+        order.Status = OrderStatus.PENDING;
+        await _orderRepository.AddAsync(order);
+        List<OrderItem> orderItems = new List<OrderItem>();
+        foreach(var orderItemDto in orderCreationDto.OrderItemCreationDtos)
+        {
+            var koiFish = await _koiFishRepository.FindAsync(x => x.Id == orderItemDto.KoiFishId && x.isDeleted == false);
+            if (koiFish == null)
+            {
+                throw new NotFoundException(ExceptionConstants.KOIFISH_NOT_EXIST + orderItemDto.KoiFishId);
+            }
+            if (koiFish.Status != KoiFishStatus.AVAILABLE)
+            {
+                throw new ValidationException(ExceptionConstants.INVALID_KOIFISH_STATUS);
+            }
+            var orderItem = _mapper.Map<OrderItem>(orderItemDto);
+            orderItem.Price = koiFish.Price; // check lại
+            orderItem.OrderId = order.Id;
+            orderItems.Add(orderItem);
+        }
+        order.OrderItems = orderItems;
+        foreach (var orderItem in orderItems)
+        {
+            await _orderItemRepository.AddAsync(orderItem);
         }
         // Total ammount -> tính tiền
-        
-        var order = _mapper.Map<Order>(orderCreationDto);
-        await _orderRepository.AddAsync(order);
+        order.TotalAmount = 10000; // chưa có func của Ngọc
+        await _orderRepository.UpdateAsync(order);
+        PaymentInformationModel paymentInformationModel = new PaymentInformationModel
+        {
+            Amount = (double)order.TotalAmount,
+            OrderId = order.Id.ToString(),
+        };
+        var httpContext = _httpContextAccessor.HttpContext;
+        var paymentUrl = _vnPayService.CreatePaymentUrl(paymentInformationModel, httpContext);
+        return paymentUrl;
     }
 
     public async Task<OrderDto> GetOrderById(int id)
