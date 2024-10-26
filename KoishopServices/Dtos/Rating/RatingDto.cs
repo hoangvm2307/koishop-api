fc0503360d19b1d1b88f17f9de40617309015697
Bao Ngoc <ngocnguyen.150903@gmail.com>
2024-10-26 11:08:15 +0700

    Add field Rating

--- KoishopServices/Dtos/Rating/RatingDto.cs
+++ KoishopServices/Dtos/Rating/RatingDto.cs
@@ -1,24 +1,22 @@
 ﻿using DTOs.AccountDtos;
 using DTOs.KoiFish;
-using KoishopBusinessObjects;
-using KoishopServices.Common.Mapping;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DTOs.Rating;
-
-public class RatingDto
+namespace KoishopServices.Dtos.Rating
 {
-    public int Id { get; set; }
-    public int RatingValue { get; set; }
-    public string? Comment { get; set; }
-
-    public int UserId { get; set; }
-    public virtual UserDto? UserDto { get; set; }
-    public int KoiFishId { get; set; }
-    public virtual KoiFishDto? KoiFishDto { get; set;}
+    public class RatingDto
+    {
+        public int Id { get; set; }
+        public int RatingValue { get; set; }
+        public string? Comment { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public virtual UserDto? UserDto { get; set; }
+        public int KoiFishId { get; set; }
+        public virtual KoiFishDto? KoiFishDto { get; set;}
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool? isDeleted { get; set; } = false;
+        public string? CreatedBy { get; set; }
+        public string? ModifiedBy { get; set; }
+    }
 }
