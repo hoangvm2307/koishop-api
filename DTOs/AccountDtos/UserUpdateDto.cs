97a08b1c582517666f3e21ed29dbbe9744df4bdc
Bao Ngoc <ngocnguyen.150903@gmail.com>
2024-11-07 10:47:42 +0700

    Handle staff

---
+++ DTOs/AccountDtos/UserUpdateDto.cs
@@ -0,0 +1,11 @@
+using KoishopBusinessObjects;
+
+namespace DTOs.AccountDtos
+{
+  public class UserUpdateDto
+  {
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string PhoneNumber { get; set; }
+  }
+}
